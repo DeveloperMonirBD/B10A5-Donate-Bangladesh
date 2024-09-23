a570690ee406f438471f52d8b2001a1c96c467da
Monir <mrmonir0558@gmail.com>
2024-09-23 18:01:27 +0600

    Donation and History button functionality done

--- js/script.js
+++ js/script.js
@@ -1,1 +1,51 @@
+//history tab functionality :
+const historyTab = document.getElementById("history-tab");
+const donationTab = document.getElementById("donation-tab");
 
+historyTab.addEventListener("click", function () {
+    historyTab.classList.add("text-black", "bg-lime-400", "hover:bg-lime-500");
+    historyTab.classList.remove(
+        "text-gray-600",
+        "border-gray-300",
+        "hover:border-gray-300"
+    );
+
+    donationTab.classList.remove(
+        "text-black",
+        "bg-lime-400",
+        "hover:bg-lime-500",
+        "border-lime-300",
+        "hover:border-lime-300"
+    );
+    donationTab.classList.add(
+        "text-gray-600",
+        "border-gray-300",
+        "hover:border-gray-300"
+    );
+    document.getElementById("card-section").classList.add("hidden");
+    document.getElementById("history-section").classList.remove("hidden");
+});
+
+donationTab.addEventListener("click", function () {
+    donationTab.classList.add("text-black", "bg-lime-400", "hover:bg-lime-500");
+    donationTab.classList.remove(
+        "text-gray-600",
+        "border-gray-300",
+        "hover:border-gray-300"
+    );
+
+    historyTab.classList.remove(
+        "text-black",
+        "bg-lime-400",
+        "hover:bg-lime-500",
+        "border-lime-300",
+        "hover:border-lime-300"
+    );
+    historyTab.classList.add(
+        "text-gray-600",
+        "border-gray-300",
+        "hover:border-gray-300"
+    );
+    document.getElementById("card-section").classList.remove("hidden");
+    document.getElementById("history-section").classList.add("hidden");
+});
